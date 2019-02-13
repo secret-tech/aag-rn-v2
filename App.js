b1810d269d0c34f3ef2751bf627327ba35e35313
Aidar Ibatullin <amazing.space.invader@gmail.com>
2019-02-13 17:26:54 +0300

    Added onesignal dep

--- App.js
+++ App.js
@@ -1,32 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { LoginButton } from 'react-native-fbsdk';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import OneSignal from 'react-native-onesignal';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  componentDidMount() {
+    OneSignal.setLogLevel(7, 0);
+    OneSignal.init('96ef2fff-208d-4b7e-a592-6c45b3633fad', { kOSSettingsKeyAutoPrompt: true });
+    OneSignal.inFocusDisplaying(0);
+
+    OneSignal.addEventListener('received', (n) => console.log('received', n));
+    OneSignal.addEventListener('opened', (n) => console.log('opened', n));
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received');
+    OneSignal.removeEventListener('opened');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-
         <View>
           <LoginButton 
             publishPermissions={['publish_actions']}
@@ -45,15 +40,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });

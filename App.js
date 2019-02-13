a22da7315c9ee1f6793e2562a456ee9324b1cfde
Aidar Ibatullin <amazing.space.invader@gmail.com>
2019-02-13 20:05:52 +0300

    Added webrtc

--- App.js
+++ App.js
@@ -3,31 +3,93 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 import { LoginButton } from 'react-native-fbsdk';
 
 import OneSignal from 'react-native-onesignal';
+import { RTCPeerConnection, RTCMediaStream, RTCIceCandidate, RTCSessionDescription, RTCView, MediaStreamTrack, getUserMedia } from 'react-native-webrtc';
+
+// Конфигурация локальных медиа данных
+const LOCAL_STREAM_OPTIONS = {
+  audio: true,
+  video: {
+    facingMode: 'user',
+    mandatory: {
+      minWidth: 600,
+      minHeight: 800,
+      minFrameRate: 30
+    }
+  }
+};
+
+// Устаревшие опции для создания offer. По-умолчанию они являются true, а тут находятся на всякий случай
+const OFFER_OPTIONS = {
+  offerToReceiveAudio: true,
+  offerToReceiveVideo: true
+};
 
 export default class App extends Component {
-  componentDidMount() {
-    OneSignal.setLogLevel(7, 0);
-    OneSignal.init('96ef2fff-208d-4b7e-a592-6c45b3633fad', { kOSSettingsKeyAutoPrompt: true });
-    OneSignal.inFocusDisplaying(0);
+  constructor(props) {
+    super(props);
 
-    OneSignal.addEventListener('received', (n) => console.log('received', n));
-    OneSignal.addEventListener('opened', (n) => console.log('opened', n));
+    this.configuration = {
+      'iceServers': [
+        { url: 'stun:stun2.1.google.com:19302' },
+        { url: 'stun:stun1.l.google.com:19302' },
+        { url: 'stun:stun1.voiceeclipse.net:3478' },
+        { url: 'stun:stun2.l.google.com:19302' },
+        { url: 'stun:stun3.l.google.com:19302' },
+        { url: 'stun:stun4.l.google.com:19302' }
+      ]
+    };
+
+    this.localPeer = null;
+
+    this.state = {
+      localStream: null,
+      remoteStream: null
+    };
   }
 
-  componentWillUnmount() {
-    OneSignal.removeEventListener('received');
-    OneSignal.removeEventListener('opened');
+  componentDidMount() {
+    // 1 Создаем RTC Peer Connection
+    this.localPeer = new RTCPeerConnection(this.configuration);
+
+    // 2 Handle ICE candidates and sync that shit between peers via socket
+    // this.localPeer.onicecandidate = ({ candidate }) => {
+    //   const body = { conversationId: this.props.navigation.state.params.conversationId, candidate };
+    //   if (candidate) this.socket.emit('req:ice', body);
+    // };
+
+    // Create local stream (video, audio)
+    getUserMedia(
+      LOCAL_STREAM_OPTIONS, 
+      (stream) => {
+        console.log(this.localPeer, stream);
+        this.setState({ localStream: stream });
+        this.localPeer.addStream(stream);
+      }, 
+      console.log
+    );
   }
 
   render() {
+    const { localStream } = this.state;
+
     return (
       <View style={styles.container}>
-        <View>
-          <LoginButton 
-            publishPermissions={['publish_actions']}
-            onLoginFinished={console.log}
-            onLogoutFinished={console.log}
-          />
+        <View style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          backgroundColor: 'blue'
+        }}>
+          <RTCView
+            objectFit="cover" 
+            style={{
+              width: '100%',
+              height: '100%',
+              flex: 1
+            }} 
+            streamURL={localStream && localStream.toURL()}/>
         </View>
       </View>
     );
@@ -36,9 +98,6 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
   }
 });
